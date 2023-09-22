cc631e7ca9eb8cfe4c981183a0127fd12ba44a71
Alexandra Szvatko <alexandraszvatko@gmail.com>
2023-09-22 19:19:10 +0200

    fetch

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-function negativeOrPositive(number) {
-    if (number < 0) {
-        return "negative"
-    } else if (number > 0) {
-        return "positive"
-    } else {
-        return "zero"
-    }
-}
+console.log('loaded');
 
-function logger(text) {
-    console.log(text)
-}
-
-/* logger(negativeOrPositive(0)) */
-
-function decideIfBeerIsGood(beer) {
-    if (beer.price < 340 && beer.abv > 4.7) {
-        return beer.name + ": jó sör mert olcsó és üt"
-    } else if (beer.price < 340) {
-        return beer.name + ": jó sör, mert olcsó"
-    } else if (beer.abv > 4.7) { 
-        return beer.name + ": jó sör mert üt"
-    } else {
-        return beer.name + ": nem jó"
-    }
-}
-
-let dreher = { name: "Dreher", price: 349, abv: 5}
-let soproni = { name: "Soproni", price: 329, abv:4.5}
-let borsodi = { name: "Borsodi", price: 339, abv: 4.8}
-let pecsi = { name: "Pécsi", price: 379, abv: 4.2}
+let rootElement = document.querySelector('#root')
+//console.log(rootElement)
 
 
-logger(decideIfBeerIsGood(borsodi))
-logger(decideIfBeerIsGood(soproni))
-logger(decideIfBeerIsGood(dreher))
-logger(decideIfBeerIsGood(pecsi))
+/* let htmlContent = */
+
+/* let counter= 0;
+while (counter <20) {
+rootElement.insertAdjacentElement('beforeend', htmlContent);
+counter++;
+} */
+
+/* for (let counter = 1; counter <10; counter++) {
+    rootElement.insertAdjacentHTML("beforeend", htmlContent);
+} */
+
+fetch('https://restcountries.com/v3.1/all')
+.then(function(response) { 
+    return response.json()
+})
+.then(function(data){
+    console.log(data)  // ITT TUDUNK A KAPOTT ADATTAL DOLGOZNI
+                       // A DATA EGY 250 ELEMBŐL ÁLLÓ ARRAY
+
+    for (let index = 0; index < data.length; index++) {
+        let countryName = data[index].name.common
+        let countryPop = data[index].population
+        let countryArea = data[index].area
+
+        rootElement.insertAdjacentHTML('beforeend', `
+        <div class = "country">
+            <h2> ${countryName} </h2>
+            <p2> population: ${countryPop} </p>
+            <p> area: ${countryArea} km2 </p>
+        </div>
+        `
+        );
+    }   
+});
